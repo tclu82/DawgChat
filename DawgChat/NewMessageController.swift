b151b29cc936dfacf9d337f76df33085dbdabf75
Zac <tclu82@uw.edu>
2017-02-27 14:10:47 -0800

    Set profile rounded image and using cache without redownloading

--- DawgChat/NewMessageController.swift
+++ DawgChat/NewMessageController.swift
@@ -81,45 +81,101 @@ class NewMessageController: UITableViewController {
         
 //        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserCell
         let user = users[indexPath.row]
         cell.textLabel?.text = user.name
         cell.detailTextLabel?.text = user.email
         
-        
+//        // Load image for each user
 //        cell.imageView?.image = UIImage(named: "cat")
+//        // Ratio is not fit for profile
+//        cell.imageView?.contentMode = .scaleAspectFill
         
         // Use image from Firebase for user profile display
         if let profileImageUrl = user.profileImageUrl
         {
-            let url = NSURL(string: profileImageUrl)
-            URLSession.shared.dataTask(with: url as! URL, completionHandler: { (data, response, err) in
-                
-                // Download failed!
-                if err != nil
-                {
-                    print(err!)
-                    return
-                }
-                DispatchQueue.main.async {
-                    cell.imageView?.image = UIImage(data: data!)
-                }
-              
-            }).resume()
+            // Load image from Extensions.swift for cache handling
+            cell.profileImageView.loadImageUsingCacheWithUrlString(urlString: profileImageUrl)
+            
+            // Need to use cache to reduce data usage
+//            let url = NSURL(string: profileImageUrl)
+//            URLSession.shared.dataTask(with: url as! URL, completionHandler: { (data, response, err) in
+//                
+//                // Download failed!
+//                if err != nil
+//                {
+//                    print(err!)
+//                    return
+//                }
+//                DispatchQueue.main.async {
+//                    cell.profileImageView.image = UIImage(data: data!)
+////                    cell.imageView?.image = UIImage(data: data!)
+//                }
+//              
+//            }).resume()
         }
-        
-        
-        
-        
         return cell
+    }
+    
+    /// Change the Row height
+    ///
+    /// - Parameters:
+    ///   - tableView: <#tableView description#>
+    ///   - indexPath: <#indexPath description#>
+    /// - Returns: <#return value description#>
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 64
     }
 }
 
 /// Inner class for UITableViewCell for User
 class UserCell: UITableViewCell {
+
+    var profileImageViewRadius: Int = { return 20 }()
     
+    // A property for customize profile image view
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+//        imageView.image = UIImage(named: "cat")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        // Radius = height / 2
+        imageView.layer.cornerRadius = 25
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    
+    /// This func set profileImageView anchors
+    ///
+    /// - Parameters:
+    ///   - style: style description
+    ///   - reuseIdentifier: reuseIdentifier description
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        addSubview(profileImageView)
+        
+        // Add constrait anchors for x, y, width and height
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 7).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+    }
+    
+    
+    /// Set text label and detail text label layout
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // Text label starts 7 pixels after image (64 - 7(leftAnchor) - 50(width) = 7)
+        // -/+ 2 creates gap between text label and detail text lable
+        textLabel?.frame = CGRect(x: 64, y: textLabel!.frame.origin.y - 2,
+                                  width: textLabel!.frame.width,
+                                  height: textLabel!.frame.height)
+        // Details follows text label
+        detailTextLabel?.frame = CGRect(x: 64, y: detailTextLabel!.frame.origin.y + 2,
+                                        width: detailTextLabel!.frame.width,
+                                        height: detailTextLabel!.frame.height)
     }
     
     required init?(coder aDecoder: NSCoder) {
